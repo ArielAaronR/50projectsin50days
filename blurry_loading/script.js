4d3fedeafc63ff1c8e8b706781c2a81fb2b3dea7
ArielAaronR <aaronresurreccion@gmail.com>
2021-01-01 04:13:02 -0800

    blurry loading

---
+++ blurry_loading/script.js
@@ -0,0 +1,21 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+const blurring = () => {
+  load++;
+
+  load > 99 ? clearInterval(int) : '';
+
+  loadText.innerText = `${load}%`;
+  //   map sync 0%-100% to 1-0
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+let int = setInterval(blurring, 30);
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
