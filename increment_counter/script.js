e1280743536fd3b798aad6319e2452bcefe2c7a5
ArielAaronR <aaronresurreccion@gmail.com>
2021-01-11 12:24:20 -0800

    increment counter

---
+++ increment_counter/script.js
@@ -0,0 +1,24 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+  counter.innerText = '0';
+
+  const updateCouter = () => {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+
+      setTimeout(() => {
+        updateCouter();
+      }, 1);
+    } else{
+        counter.innerText = target
+    }
+  };
+
+  updateCouter();
+});
